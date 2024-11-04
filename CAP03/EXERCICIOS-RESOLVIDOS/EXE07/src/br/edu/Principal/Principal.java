9c39afc43e587dd2f8d65a05a0c8d47772b51aaf
Gabriel Abreu <161750391+Gabrielzab@users.noreply.github.com>
2024-11-04 14:34:04 -0300

    Add files via upload

---
+++ CAP03/EXERCICIOS-RESOLVIDOS/EXE07/src/br/edu/Principal/Principal.java
@@ -0,0 +1,13 @@
+package br.edu.Principal;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		  double salario = 1000;
+	        double imposto = salario * 0.10;
+	        double salario_receber = salario + 50 - imposto;
+	        System.out.println("O salário a receber é de " + salario_receber);
+
+	}
+
+}
