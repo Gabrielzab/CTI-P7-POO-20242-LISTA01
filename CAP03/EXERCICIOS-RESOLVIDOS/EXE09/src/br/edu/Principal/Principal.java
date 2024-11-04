66eb2b258a4dfa457a1dce918f0739ec44391538
Gabriel Abreu <161750391+Gabrielzab@users.noreply.github.com>
2024-11-04 14:39:43 -0300

    Add files via upload

---
+++ CAP03/EXERCICIOS-RESOLVIDOS/EXE09/src/br/edu/Principal/Principal.java
@@ -0,0 +1,14 @@
+package br.edu.Principal;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		double deposito = 13;
+        double juros = 5;
+        double rendimento = deposito * (juros / 100);
+        double valor_total = deposito + rendimento;
+        System.out.println("O valor do rendimento é " + rendimento);
+        System.out.println("O valor total é " + valor_total);
+	}
+
+}
