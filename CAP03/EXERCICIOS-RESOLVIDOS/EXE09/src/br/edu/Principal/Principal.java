d43e4c7e54dc04aa773d3517b78a374a6eafc0e4
Gabriel Abreu <161750391+Gabrielzab@users.noreply.github.com>
2024-11-04 14:52:06 -0300

    Add files via upload

---
+++ CAP03/EXERCICIOS-RESOLVIDOS/EXE09/src/br/edu/Principal/Principal.java
@@ -0,0 +1,12 @@
+package br.edu.Principal;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		  double base = 20;
+	        double altura = 10;
+	        double area = (base * altura)/2;
+	        System.out.print("A área do triángulo é " + area);
+	}
+
+}
