880e5c99c91390569c21939db56229962d7c227c
Gabriel Abreu <161750391+Gabrielzab@users.noreply.github.com>
2024-12-02 14:09:40 -0300

    Update Principal.java

--- CAP06/EXERCICIOS-RESOLVIDOS/EXE01/src/br/edu/principal/Principal.java
+++ CAP06/EXERCICIOS-RESOLVIDOS/EXE01/src/br/edu/principal/Principal.java
@@ -1,26 +1,26 @@
-package br.edu.principal;
 
-public class Principal {
+ package br.edu.principal;
+
+public class principal {
 
 	public static void main(String[] args) {
-		int[] num = new int[9];
-		int cont, i, j;
-		for (i = 0; i < 9; i++) {
+        int[] num = new int[9];
+        int cont, i, j;
 
-		}
-		for (i = 0; i < 9; i++) {
-		    cont = 0;
-		    for (j = 1; j <= num[i]; j++) {
-		        if (num[i] % j == 0) {
-		            cont++;
-		        }
-		    }
-		    if (cont == 2) {
+        for (i = 0; i < 9; i++) {
+            num[i] = i + 1;
+        }
 
-		        System.out.println(num[i] + " está na posição " + (i + 1));
-		    }
-		}
-
-	}
-
+        for (i = 0; i < 9; i++) {
+            cont = 0;
+            for (j = 1; j <= num[i]; j++) {
+                if (num[i] % j == 0) {
+                    cont++;
+                }
+            }
+            if (cont == 2) {
+                System.out.println(num[i] + " está na posição " + (i + 1));
+            }
+        }
+    }
 }
